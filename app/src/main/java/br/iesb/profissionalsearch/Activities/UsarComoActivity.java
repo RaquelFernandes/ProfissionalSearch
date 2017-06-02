ecd72ba20041b88171f62d1e0f527c54dafa1de4
Raquel Fernandes <1514290042@iesb.edu.br>
2017-06-01 22:01:40 -0300

    Profissional Search - Cadastrar Usuario

--- app/src/main/java/br/iesb/profissionalsearch/Activities/UsarComoActivity.java
+++ app/src/main/java/br/iesb/profissionalsearch/Activities/UsarComoActivity.java
@@ -1,28 +1,60 @@
 package br.iesb.profissionalsearch.Activities;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
 
 import br.iesb.profissionalsearch.R;
 
 public class UsarComoActivity extends AppCompatActivity {
 
+    private DatabaseReference database;
+    FirebaseAuth auth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_usar_como);
+
+        autenticar();
     }
 
-    private void autenticar(){
+    private void autenticar() {
 
-        EditText txtEmail = (EditText) findViewById(R.id.login_txtEmail);
-        EditText txtSenha = (EditText) findViewById(R.id.login_txtSenha);
+        auth = FirebaseAuth.getInstance();
 
-
+        final EditText txtEmail = (EditText) findViewById(R.id.login_txtEmail);
+        final EditText txtSenha = (EditText) findViewById(R.id.login_txtSenha);
 
         Button btnEntrar = (Button) findViewById(R.id.login_btnEntrar);
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                auth.createUserWithEmailAndPassword(txtEmail.getText().toString(), txtSenha.getText().toString())
+                    .addOnCompleteListener(UsarComoActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(UsarComoActivity.this, "Cadastrado com sucesso", Toast.LENGTH_SHORT).show();
+
+                            } else {
+                                Toast.makeText(UsarComoActivity.this, "Erro ao ser cadastrado", Toast.LENGTH_SHORT).show();
+                            }
+
+                        }
+                    });
+            }
+        });
 
     }
 
